c8136401dbf372d9a53d89706db6158d566cf1fb
Adrian Śliwa <adrian@adrenalin.io>
2016-11-13 12:44:52 +0100

    Goes to map screen

---
+++ Assets/Sripts/LoadOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(string sceneName) {
+		SceneManager.LoadScene (sceneName);	
+	}
+}
