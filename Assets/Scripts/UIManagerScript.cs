8170c7db6cf3f2623d9c56ee940c9664795da037
Adrian Śliwa <adrian@adrenalin.io>
2017-01-08 14:06:47 +0100

    Creates maps panel and opens it

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -3,8 +3,16 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
-	
+
+	public Animator startPanelAnimator;
+	public GameObject mapsPanel;
+
 	public void loadScene(string sceneName) {
 		SceneManager.LoadScene (sceneName);	
+	}
+
+	public void openMaps() {
+		startPanelAnimator.SetBool("isHidden", true);
+		mapsPanel.SetActive(true);
 	}
 }
