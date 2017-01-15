d2a4ead357fcb05d4ddb0af922d002c806ce6913
Adrian Śliwa <adrian@adrenalin.io>
2017-01-15 19:35:35 +0100

    Slides maps from left to the centre

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -2,19 +2,27 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class UIManagerScript : MonoBehaviour {
+public class UIManagerScript : MonoBehaviour
+{
 
 	public Animator startPanelAnimator;
 	public Animator mapsPanelAnimator;
 	public GameObject mapsPanel;
 
-	public void loadScene(string sceneName) {
+	void Start ()
+	{
+		mapsPanel.SetActive (false);
+	}
+
+	public void loadScene (string sceneName)
+	{
 		SceneManager.LoadScene (sceneName);	
 	}
 
-	public void openMaps() {
-		startPanelAnimator.SetBool("isHidden", true);
-		mapsPanelAnimator.SetBool("isHidden", false);
-		mapsPanel.SetActive(true);
+	public void openMaps ()
+	{
+		startPanelAnimator.SetBool ("isHidden", true);
+		mapsPanel.SetActive (true);
+		mapsPanelAnimator.SetBool ("isHidden", false);
 	}
 }
