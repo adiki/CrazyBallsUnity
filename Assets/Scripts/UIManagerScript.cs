4d1bfe00aedb2276c1d7c98234e57bebeb28824c
Adrian Śliwa <adrian@adrenalin.io>
2017-02-06 19:41:33 +0100

    Clears project

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -4,26 +4,8 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour
 {
-
-	public Animator uiAnimator;
-
 	public void loadScene (string sceneName)
 	{
 		SceneManager.LoadScene (sceneName);	
-	}
-
-	public void openSelectMaps ()
-	{
-		uiAnimator.SetBool ("isSelectMapHidden", false);
-	}
-
-	public void openWoodMap ()
-	{
-		uiAnimator.SetBool ("isWoodMapHidden", false);
-	}
-
-	public void closeWoodMap ()
-	{
-		uiAnimator.SetBool ("isWoodMapHidden", true);
 	}
 }
