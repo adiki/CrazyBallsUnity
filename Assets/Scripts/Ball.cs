6ae5f3c91d38681789e130d702e9f3abbdd0f1a4
Adrian Śliwa <adrian@adrenalin.io>
2017-02-21 19:20:07 +0100

    Changes player texture and moves player

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ball : MonoBehaviour
+{
+
+	public int id;
+	//	public Vector3 velocityCached;
+	protected Rigidbody rigidBody;
+
+	//	private const float SIZE = 0.5f;
+
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		if (rigidBody.position.y < -50) {
+			resetPosition ();
+			return;
+		}
+
+		if (rigidBody.position.y < -1) {
+			return;
+		}
+
+		Move ();
+//		cacheVelocity ();
+	}
+
+	void Move ()
+	{
+		Vector3 movement = Movement ();
+
+		rigidBody.AddForce (movement, ForceMode.Impulse);
+	}
+
+	protected abstract Vector3 Movement ();
+
+	private void resetPosition ()
+	{
+		rigidBody.velocity = Vector3.zero;
+		if (id == 0) {
+			transform.position = new Vector3 (3, 1, 0);	
+		} else if (id == 1) {
+			transform.position = new Vector3 (0, 1, -3);	
+		} else if (id == 2) {
+			transform.position = new Vector3 (-3, 1, 0);	
+		} else if (id == 3) {
+			transform.position = new Vector3 (0, 1, 3);	
+		}	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Ball {
+
+	protected override Vector3 Movement() {
+		float x, z;
+		x = Input.acceleration.x;
+		z = Input.acceleration.y;
+
+		Vector3 movement = new Vector3 (x, 0.0f, z);
+
+		float x2, z2;
+		x2 = Input.GetAxis ("Horizontal");
+		z2 = Input.GetAxis ("Vertical");
+
+		if (x2 != 0 || z2 != 0) {
+			movement = new Vector3 (x2, 0.0f, z2);
+		}
+
+		return movement;
+	}
+}
