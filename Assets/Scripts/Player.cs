266d8bcc8122f947d460d37d275ea29813a08e3d
Adrian Śliwa <adrian@adrenalin.io>
2017-03-12 20:27:01 +0100

    Adds pause button time text and results panels

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,24 @@ public class Player : Ball {
 
 	protected override Vector3 Movement() {
 		float x, z;
+		float limit = 0.3f;
 		x = Input.acceleration.x;
+		x = Mathf.Min (Mathf.Max (-limit, x), limit);
 		z = Input.acceleration.y;
+		z = Mathf.Min (Mathf.Max (-limit, z), limit);
 
 		Vector3 movement = new Vector3 (x, 0.0f, z);
 
 		float x2, z2;
 		x2 = Input.GetAxis ("Horizontal");
+		x2 = Mathf.Min (Mathf.Max (-limit, x2), limit);
 		z2 = Input.GetAxis ("Vertical");
+		z2 = Mathf.Min (Mathf.Max (-limit, z2), limit);
 
 		if (x2 != 0 || z2 != 0) {
 			movement = new Vector3 (x2, 0.0f, z2);
 		}
 
-		return movement;
+		return movement * 3f;
 	}
 }
