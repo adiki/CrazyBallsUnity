dd14aedac92f3a5cd3beefd11a2e144a429417e7
Adrian Śliwa <adrian@adrenalin.io>
2017-03-30 16:05:13 +0200

    Implements levels up to 15

---
+++ Assets/Scripts/Screens/Levels/TwoMinutesLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoMinutesLevel : Level
+{
+	protected override int gameTime () 
+	{
+		return 120;
+	}
+
+	protected override int pointsForStar1 ()
+	{
+		return 10;
+	}
+
+	protected override int pointsForStar2 ()
+	{
+		return 15;
+	}
+
+	protected override int pointsForStar3 ()
+	{
+		return 20;
+	}
+}
